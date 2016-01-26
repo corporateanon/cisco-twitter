273d642d6b885a422c0312293d87d13045e50b7e
corporate-anon <corporateanon@gmail.com>
2016-01-26 15:01:23 +0200

    changes

--- src/routes.php
+++ src/routes.php
@@ -2,28 +2,19 @@
 
 use \nanotwi\Tweet;
 
+
 $app->get('/', function ($req, $res, $args) {
-        
 
-        if ($this->cache->contains('timeline')) {
-                $statuses = $this->cache->fetch('timeline');
-        } else {
-                $statuses = $this->twitter->request('statuses/home_timeline', 'GET', array('count' => 100));
-                $this->cache->save('timeline', $statuses, 60);
-        }
-
-        $tweets = array_map(function ($status) {
-                return new Tweet($status);
-        }, $statuses);
-    
-        return $this->view->render($res, 'timeline.html', array('statuses' => $tweets));
+        return $this->view
+                ->render($res, 'index.xml')
+                ->withHeader('Content-Type', 'text/xml');
 });
 
-$app->post('/timeline-force-refresh', function ($req, $res, $args) {
-        $this->cache->delete('timeline');
-        return $res
-                ->withStatus(301)
-                ->withHeader('Location', '/');
+$app->get('/services/test', function ($req, $res, $args) {
+
+        return $this->view
+                ->render($res, 'test.xml')
+                ->withHeader('Content-Type', 'text/xml');
 });
 
 $app->get('/services/timeline', function ($req, $res, $args) {
@@ -44,3 +35,29 @@ $app->get('/services/timeline', function ($req, $res, $args) {
                 ->render($res, 'timeline.xml', array('statuses' => $tweets))
                 ->withHeader('Content-Type', 'text/xml');
 });
+
+
+// $app->get('/', function ($req, $res, $args) {
+        
+
+//         if ($this->cache->contains('timeline')) {
+//                 $statuses = $this->cache->fetch('timeline');
+//         } else {
+//                 $statuses = $this->twitter->request('statuses/home_timeline', 'GET', array('count' => 100));
+//                 $this->cache->save('timeline', $statuses, 60);
+//         }
+
+//         $tweets = array_map(function ($status) {
+//                 return new Tweet($status);
+//         }, $statuses);
+    
+//         return $this->view->render($res, 'timeline.html', array('statuses' => $tweets));
+// });
+
+// $app->post('/timeline-force-refresh', function ($req, $res, $args) {
+//         $this->cache->delete('timeline');
+//         return $res
+//                 ->withStatus(301)
+//                 ->withHeader('Location', '/');
+// });
+
