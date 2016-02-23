a17c8b9ae7434c70929a3ce1636381d762ad48b3
corporate-anon <corporateanon@gmail.com>
2016-02-23 23:10:31 +0200

    it-works

--- src/routes.php
+++ src/routes.php
@@ -1,35 +1,61 @@
 <?php
 
 use \nanotwi\Tweet;
+use Psr\Http\Message\ServerRequestInterface;
 
 
 $app->get('/', function ($req, $res, $args) {
 
-        $services = [
-                [ 'name' => 'Twitter', 'route' => 'timeline' ],
-        ];
+  $services = [
+    [ 'name' => 'Twitter', 'route' => 'timeline' ],
+  ];
 
-        return $this->view
-                ->render($res, 'index.xml', [ 'services' => $services ])
-                ->withHeader('Content-Type', 'text/xml');
+  return $this->view
+    ->render($res, 'index.xml', [ 'services' => $services ])
+    ->withHeader('Content-Type', 'text/xml');
 });
 
 
 $app->get('/services/timeline', function ($req, $res, $args) {
-        
-        $tweets = $this->nanoTwitter->getTimeline();
 
-        return $this->view
-                ->render($res, 'timeline.xml', [ 'statuses' => $tweets ] )
-                ->withHeader('Content-Type', 'text/xml');
+  $tweets = $this->nanoTwitter->getTimeline();
+
+  return $this->view
+    ->render($res, 'timeline.xml', [ 'statuses' => $tweets ] )
+    ->withHeader('Content-Type', 'text/xml');
 })->setName('timeline');
 
 
 $app->get('/services/tweet/{idStr}', function ($req, $res, $args) {
-        
-        $tweet = $this->nanoTwitter->getTweet($args['idStr']);
 
-        return $this->view
-                ->render($res, 'tweet.xml', [ 'tweet' => $tweet ] )
-                ->withHeader('Content-Type', 'text/xml');
+  $tweet = $this->nanoTwitter->getTweet($args['idStr']);
+
+  return $this->view
+    ->render($res, 'tweet.xml', [ 'tweet' => $tweet ] )
+    ->withHeader('Content-Type', 'text/xml');
 })->setName('tweet');
+
+
+$app->get('/services/compose', function ($req, $res, $args) {
+
+  return $this->view
+    ->render($res, 'compose.xml', [ ] )
+    ->withHeader('Content-Type', 'text/xml');
+})->setName('compose');
+
+
+$app->get('/services/createTweet', function (ServerRequestInterface $req, $res) {
+
+  try {
+    $params = $req->getQueryParams();
+    $this->nanoTwitter->createTweet($params['statusText']);
+    return $this->view
+      ->render($res, 'compose-success.xml', [ ] )
+      ->withHeader('Content-Type', 'text/xml');
+  } catch (Exception $e) {
+    return $this->view
+      ->render($res, 'compose-error.xml', [ 'error' => $e ] )
+      ->withHeader('Content-Type', 'text/xml');
+  }
+
+})->setName('create_tweet');
