b7895f111b36a47e3ce8e6eb6210e49c7b79c5b3
corporate-anon <corporateanon@gmail.com>
2016-04-20 10:33:30 +0300

    hex

--- lib/AutoLogin.php
+++ lib/AutoLogin.php
@@ -26,15 +26,18 @@ class AutoLogin {
 
     public function createAutologinToken(TwitterUser $user) {
         $tokens = $user->oAuthToken . self::SEPARATOR . $user->oAuthTokenSecret;
-        $keyHash = md5($this->autoLoginKey, true);
-        $base64 = openssl_encrypt($tokens, self::ENCRYPTION_METHOD, $keyHash);
-        return urlencode($base64);
+        $keyHash = \md5($this->autoLoginKey, true);
+        $encrypted = \openssl_encrypt($tokens, self::ENCRYPTION_METHOD, $keyHash, \OPENSSL_RAW_DATA);
+        return \bin2hex($encrypted);
     }
 
     public function parseAutologinToken($token) {
         $keyHash = md5($this->autoLoginKey, true);
-        $base64 = urldecode($token);
-        $tokens = openssl_decrypt($base64, self::ENCRYPTION_METHOD, $keyHash);
+        if(!preg_match('/^[0-9a-fA-F]+$/', $token)) {
+            throw new AutologinException('Could not parse auto-login token');
+        }
+        $encrypted = \hex2bin($token);
+        $tokens = \openssl_decrypt($encrypted, self::ENCRYPTION_METHOD, $keyHash, \OPENSSL_RAW_DATA);
         if ($tokens === false) {
             throw new AutologinException('Could not decrypt auto-login token');
         }
